c63ab714e06afc9811ac68d8f673620e82a3ebb7
dennyamiruddin <denny_amiruddin@y7mail.com>
2017-06-09 11:54:53 +1000

    Add files via upload

---
+++ Puck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Pong
+{
+    public class Puck
+    {
+        private int xPos, yPos;
+        private char shape;
+
+        public Puck()
+        {
+            this.xPos = 0;
+            this.yPos = 0;
+            this.shape = 'o';
+        }
+
+		public int XPos
+		{
+			get
+			{
+				return this.xPos;
+			}
+			set
+			{
+				this.xPos = value;
+			}
+		}
+
+		public int YPos
+		{
+			get
+			{
+				return this.yPos;
+			}
+			set
+			{
+				this.yPos = value;
+			}
+		}
+
+		public char Shape
+		{
+			get
+			{
+                return this.shape;
+			}
+		}
+
+    }
+}
