1cfa695c0a91067c0fcf944e39a7c91616991048
dennyamiruddin <denny_amiruddin@y7mail.com>
2017-06-09 11:54:32 +1000

    Add files via upload

---
+++ Handle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Pong
+{
+    class Handle
+    {
+        private int xPos, yPos;
+        private int height, width;
+        private char shape;
+
+        public Handle(char shape){
+            this.height = 3;
+            this.width = 1;
+            this.shape = shape;
+        }
+
+        public int XPos {
+            get
+            {
+                return this.xPos;
+            }
+			set
+			{
+                this.xPos = value;
+			}
+        }
+
+		public int YPos
+		{
+			get
+			{
+				return this.yPos;
+			}
+			set
+			{
+				this.yPos = value;
+			}
+		}
+
+		public int Height
+		{
+			get
+			{
+                return this.height;
+			}
+		}
+
+		public int Width
+		{
+			get
+			{
+                return this.width;
+			}
+		}
+
+
+		public char Shape
+		{
+			get
+			{
+                return this.shape;
+			}
+			set
+			{
+				this.shape = value;
+			}
+		}
+    }
+}
